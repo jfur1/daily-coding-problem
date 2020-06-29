6e4fe925511297824b988f6fdbd86064926803b2
John Furlong <johnfurlong24@gmail.com>
2020-06-29 15:36:03 -0600

    Catching up on some daily coding problems

--- 06-19-2020.py
+++ 06-19-2020.py
@@ -2,3 +2,27 @@
 # using the minimum number of insertions and deletions. If there are multiple solutions, return any of them.
 
 # For example, given "(()", you could return "(())". Given "))()(", you could return "()()()()".
+# Author: John Furlong
+# Description: Problems like this are similar to checking for permuations. The simplest appraoch would be
+# to keep track of the offset in symmetry of the string. 
+def balancedString(str):
+    count = 0
+    res = 0
+    for i in range(len(str)):
+        if(str[i] == ')'):
+            count += 1
+        else:
+            count -= 1
+        # Count must be >= -1 because of symmetry
+        if(count == -1):
+            count += 1
+            res += 1
+    nPairs = int(len(str)+(res+count)/2)
+
+    for i in range(0, nPairs-2):
+        print('()', end ='')
+    print('()')
+    print('Minimum # of operations to construct:', count+res)
+    return #count + res
+
+balancedString("))()(")
--- 06-20-2020.py
+++ 06-20-2020.py
@@ -7,3 +7,80 @@
 # All other characters must be lowercase letters, separators (,,;,:) or terminal marks (.,?,!,‽).
 # There must be a single space between each word.
 # The sentence must end with a terminal mark immediately following a word.
+
+def sentenceChecker(sentence):
+    seperators = [',' , ':', ';']
+    terminals = ['.' , '?', '!']
+
+    # Base Case: Is first letter capitalized?
+    if ord(sentence[0]) < 64 or ord(sentence[0]) > 90:
+        print("First character must be capital letter!")
+        return False
+
+    i = 0
+    wordLen = 1
+    terminalIdx = 1
+    while i < len(sentence)-1:
+        i += 1
+        # Terminal 
+        if sentence[i] in terminals:
+            # Was the previous character invalid?
+            if sentence[i-1] == ' ' or sentence[i-1] in seperators or sentence[i-1] in terminals:
+                print('Invalid character preceeding terminal')
+                return False
+            else:
+                wordLen = 0
+                terminalIdx = i
+                continue
+
+        # No double spaces
+        elif sentence[i] == ' ':
+            if sentence[i-1] == ' ' or sentence[i+1] == ' ':
+                print('No double spaces!')
+                return False
+            else:
+                wordLen = 0
+                continue
+        
+        # Seperator
+        elif sentence[i] in seperators:
+            if ord(sentence[i-1]) >= 97 and ord(sentence[i-1]) <= 122:
+                continue
+            else:
+                print('Invalid seperator')
+                return False
+
+        # lowercase letters
+        elif ord(sentence[i]) >= 97 and ord(sentence[i]) <= 122 :
+            if wordLen == 1 and ord(sentence[i-1]) >= 65 and ord(sentence[i-1]) <= 90:
+                wordLen += 1
+                continue
+            elif wordLen >= 1 and ord(sentence[i-1]) >= 97 and ord(sentence[i-1]) <= 122 and i > terminalIdx:
+                wordLen += 1
+                continue
+            elif wordLen == 0 and sentence[i-1] == ' ':
+                wordLen += 1
+                continue
+            else:
+                print('Invalid lowercase: ', sentence[i], i)
+                return False
+
+        wordLen += 1
+    # Make sure that the sentence(s) end with terminal
+    if sentence[len(sentence)-1] not in terminals:
+        print('Sentence must end with a terminal character!')
+        return False
+    else:   
+        print('Valid Sentence:', sentence)
+        return True
+        
+print('Test 1:')
+sentenceChecker("Hello, my name is john.")
+print('Test 2:')
+sentenceChecker("Hello, my name is john .")
+print('Test 3:')
+sentenceChecker(" Hello, my name is john.")
+print('Test 4:')
+sentenceChecker("Hello,my name is john.")
+print('Test 5:')
+sentenceChecker("Hello, my name is john")
