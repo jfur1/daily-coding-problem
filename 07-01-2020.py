457eafafe6b3adf68926fd940ad2a1eb3858ab2c
John Furlong <johnfurlong24@gmail.com>
2020-07-01 14:30:23 -0600

    review polymorphism

--- 07-01-2020.py
+++ 07-01-2020.py
@@ -5,8 +5,17 @@
 # Subtype polymorphism
 # -----------------------------------
 
-# 1. Ad-hoc Polymorphism
+# 1. Ad-hoc Polymorphism:
+#   The term refers to functions that can be applied to arguments of different
+# types, but behave differently depending on the type of argument to which
+# they are applied.
 
-# 2. Parametric Polymorphism
 
-# 3. Subtype Polymorphism
+# 2. Parametric Polymorphism:
+#   The term refers to functions that allow data types to be written generically,
+# so that is can handle values uniformly without depending on their type.
+
+
+# 3. Subtype Polymorphism:
+#   Also called inclusion polymorphism, this restricts the range of types that can
+# be used in a particular case of polymorphism.
