0388319f27563b6b5ea2cd81a15d585337835ca6
John Furlong <johnfurlong24@gmail.com>
2020-07-31 09:49:17 -0600

    daily coding problem

--- 07-2020/07-28-2020.py
+++ 07-2020/07-28-2020.py
@@ -1,3 +1,24 @@
 # Good morning! Here's your coding interview problem for today.
 
 # Given a real number n, find the square root of n. For example, given n = 9, return 3.
+# Citation: Geeksforgeeks
+# # Returns floor of square root of x 
+def floorSqrt(x): 
+  
+    # Base cases 
+    if (x == 0 or x == 1): 
+        return x 
+  
+    # Staring from 1, try all numbers until 
+    # i*i is greater than or equal to x. 
+    i = 1; result = 1
+    while (result <= x): 
+      
+        i += 1
+        result = i * i 
+      
+    return i - 1
+  
+# Driver Code 
+x = 11
+print(floorSqrt(x))
