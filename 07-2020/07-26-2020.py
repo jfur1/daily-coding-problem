e2d38272f4e5f5733b8537774206333908a2c095
John Furlong <johnfurlong24@gmail.com>
2020-08-05 08:29:34 -0600

    Daily coding problem

--- 07-2020/07-26-2020.py
+++ 07-2020/07-26-2020.py
@@ -3,3 +3,50 @@
 # This problem was asked by Google.
 
 # Given the head of a singly linked list, reverse it in-place.
+
+# Iterative Approach:
+
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+
+    def reverse(self):
+        prev = None
+        current = self.head
+        while current is not None:
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+        self.head = prev
+    
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def printList(self):
+        temp = self.head
+        while temp:
+            print(temp.data)
+            temp = temp.next
+
+# Driver Code
+llist = LinkedList()
+llist.push(20)
+llist.push(4)
+llist.push(15)
+llist.push(85)
+
+print("Given Linked List:")
+llist.printList()
+llist.reverse()
+print("\nReversed Linked List:")
+llist.printList()
