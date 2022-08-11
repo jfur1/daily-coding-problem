451d3a995becb4f280abc59ef9d6f8fe077f7ca8
John Furlong <johnfurlong24@gmail.com>
2022-08-11 12:38:09 -0600

    daily problem

--- 07-2020/07-26-2020.py
+++ 07-2020/07-26-2020.py
@@ -4,7 +4,9 @@
 
 # Given the head of a singly linked list, reverse it in-place.
 
-# Iterative Approach:
+# Iterative and Recusive Solutions included:
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 
 class Node:
 
@@ -26,6 +28,17 @@ class LinkedList:
             prev = current
             current = next
         self.head = prev
+
+    def recursiveReverse(self, head):
+        if head is None or head.next is None:
+            return head
+
+        rest = self.recursiveReverse(head.next)
+
+        head.next.next = head
+        head.next = None
+
+        return rest
     
     def push(self, new_data):
         new_node = Node(new_data)
@@ -44,9 +57,22 @@ llist.push(20)
 llist.push(4)
 llist.push(15)
 llist.push(85)
-
+print("Iterative Solution:")
 print("Given Linked List:")
 llist.printList()
 llist.reverse()
 print("\nReversed Linked List:")
 llist.printList()
+print()
+# Recursive Implementation Driver Code
+llist2 = LinkedList()
+llist2.push(20)
+llist2.push(4)
+llist2.push(15)
+llist2.push(85)
+print("Recursive Solution:")
+print("Given Linked List:")
+llist2.printList()
+llist2.head = llist2.recursiveReverse(llist2.head)
+print("Reversed Linked List:")
+llist2.printList()
